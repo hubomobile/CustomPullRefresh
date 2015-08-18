f4ff14b411dad3b1b912a6fb5afdb77352c06d28
hubo <hubo10086@163.om>
2015-08-18 10:54:30 +0800

    Commit a Demo Project & Core.

---
+++ CustomPullRefreshDemo/CustomPullRefreshDemo/CustomPullRefresh/UIScrollView+CustomPullRefresh.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+CustomPullRefresh.h
+//  CustomPullRefreshDemo
+//
+//  Created by 胡波 on 15/8/18.
+//  Copyright (c) 2015年 hubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (CustomPullRefresh)
+
+@end
---
+++ CustomPullRefreshDemo/CustomPullRefreshDemo/CustomPullRefresh/UIScrollView+CustomPullRefresh.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+CustomPullRefresh.m
+//  CustomPullRefreshDemo
+//
+//  Created by 胡波 on 15/8/18.
+//  Copyright (c) 2015年 hubo. All rights reserved.
+//
+
+#import "UIScrollView+CustomPullRefresh.h"
+
+@implementation UIScrollView (CustomPullRefresh)
+
+@end
